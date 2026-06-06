ebf9f74d34082adec28c900af4fa2628a4c1f3ad
Kristel Tammel <kristeltube@gmail.com>
2026-06-06 09:19:07 +0300

    Kustuta fail

--- scripts/dev_reset_merit_staging.sql
+++
@@ -1,9 +0,0 @@
--- DEVELOPMENT ONLY!
--- This script deletes Merit staging tables.
--- Do not use this in regular or production loads.
-
-DROP TABLE IF EXISTS staging.merit_payments_raw;
-DROP TABLE IF EXISTS staging.merit_sales_invoices_raw;
-DROP TABLE IF EXISTS staging.merit_purchase_invoices_raw;
-DROP TABLE IF EXISTS staging.merit_vendors_raw;
-DROP TABLE IF EXISTS staging.merit_customers_raw;
